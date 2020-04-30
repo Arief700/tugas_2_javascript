560bd6284206dbb0538aaf5a0d8bbd0687e72834
Arief Setiawan <Aremor14@gmail.com>
2020-05-01 01:24:17 +0700

    Tugas 02 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,20 @@
+// tipe data string
+var string = "Niomic"
+
+// tipe data number
+var number = 2020;
+
+// tipe data boolean
+var boolean = true;
+
+// tipe data null
+var test = null;
+
+// tipe data array
+var arr = [1,2,3, 'Niomic', true];
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(test);
+console.log(arr);
